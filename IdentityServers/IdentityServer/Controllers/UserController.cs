5e534473cf736c7627ce2d9cee1e88342482d6d8
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-08-13 13:38:25 +0300

    Added Authorize to User controller and configured Config.cs

--- IdentityServers/IdentityServer/Controllers/UserController.cs
+++ IdentityServers/IdentityServer/Controllers/UserController.cs
@@ -1,15 +1,18 @@
 ﻿using IdentityServer.Dtos;
 using IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SharedNET3.Dtos;
 using System.Linq;
 using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
 
 namespace IdentityServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UserController : ControllerBase
     {
