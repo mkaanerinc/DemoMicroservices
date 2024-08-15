0d61f5cf9f0e301ee18c66cd1c7a73cd8ff152a0
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-08-15 10:17:17 +0300

    Added BasketDto & BasketItemDto and added Options Pattern for Redis configuration

---
+++ Services/Basket/Services.Basket/Dtos/BasketDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Basket.Dtos;
+
+public class BasketDto
+{
+    public string UserId { get; set; }
+    public string DiscountCode { get; set; }
+    public List<BasketItemDto> BasketItems { get; set; }
+    public decimal TotalPrice { get => BasketItems.Sum(x => x.Price * x.Quantity); }
+}
