c41f70d57b3b1f79c604e549dfa791afdd3b1bf2
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-08-18 17:03:55 +0300

    Added microservices to gateway

--- Gateways/Gateway/Program.cs
+++ Gateways/Gateway/Program.cs
@@ -3,7 +3,11 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Configuration
+    .AddJsonFile("configuration.development.json", optional: false, reloadOnChange: true);
+    //.AddJsonFile("configuration.production.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
