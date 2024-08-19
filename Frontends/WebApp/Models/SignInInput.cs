29878ae239e61921cbc5a9ef9d98f5f13c1a05f5
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-08-19 09:13:48 +0300

    Added IIdentityService and Identity Model library

---
+++ Frontends/WebApp/Models/SignInInput.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class SignInInput
+{
+    [Display(Name = "Email adresiniz")]
+    public string Email { get; set; }
+
+    [Display(Name = "Parolanız")]
+    public string Password { get; set; }
+
+    [Display(Name = "Beni hatırla")]
+    public bool IsRemember { get; set; }
+}
---
+++ Frontends/WebApp/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using IdentityModel.Client;
+using Shared.Dtos;
+using WebApp.Models;
+
+namespace WebApp.Services.Interfaces;
+
+public interface IIdentityService
+{
+    Task<Response<bool>> SignIn(SignInInput signInInput);
+    Task<TokenResponse> GetAccessTokenByRefreshToken();
+    Task RevokeRefreshToken();
+}
